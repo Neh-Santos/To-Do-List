6e3b6f40ea35d01446e237de671744cae1c80a0a
Neh-Santos <ricleciosousa7@gmail.com>
2026-08-13 14:16:02 -0300

    Nono projeto

---
+++ calculadora/script/index.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+        function adicionar(valor) {
+            display.value += valor;
+        }
+
+        function limpar() {
+            display.value = "";
+        }
+
+        function apagar() {
+            display.value = display.value.slice(0, -1);
+        }
+
+        function calcular() {
+            display.value = eval(display.value);
+        }
